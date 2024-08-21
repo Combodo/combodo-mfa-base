b21c9e470292798f7cac5943adcce3dbef40e481
odain <olivier.dain@combodo.com>
2024-08-21 11:25:20 +0200

    ci: add FR dict file iso to EN to fix tests in jenkins

---
+++ dictionaries/fr.dict.combodo-mfa-base.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2013 XXXXX
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('EN US', 'English', 'English', array(
+	'MyAccount:Tab:MFA' => 'Multi-Factor authentication',
+	'UI:MFA' => 'MFA User settings',
+	'Class:MFAAdminRule' => 'Multi-Factor authentication rules',
+	'Class:MFAAdminRule+' => 'Multi-Factor authentication rules',
+	'Menu:MFAAdminRule' => 'MFA rules',
+	'Menu:MFAAdminRule+' => 'Multi-Factor authentication rules',
+));
