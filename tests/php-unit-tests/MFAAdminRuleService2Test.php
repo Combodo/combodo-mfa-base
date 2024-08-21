5c8de69b20c8203ebd34c46e949bad3672648ea8
odain <olivier.dain@combodo.com>
2024-08-21 16:45:03 +0200

    new test fixed

--- tests/php-unit-tests/MFAAdminRuleService2Test.php
+++ tests/php-unit-tests/MFAAdminRuleService2Test.php
@@ -5,9 +5,11 @@ namespace Combodo\iTop\MFABase\Test;
 use Combodo\iTop\MFABase\Service\MFAAdminRuleService;
 use Combodo\iTop\Test\UnitTest\ItopDataTestCase;
 
-class MFAAdminRuleServiceTest2 extends ItopDataTestCase {
-	public function setUp() : void {
-		parent:$this->setUp();
+class MFAAdminRuleService2Test extends ItopDataTestCase {
+	protected function setUp(): void
+	{
+		parent::setUp();
+		$this->RequireOnceItopFile('/env-production/combodo-mfa-base/vendor/autoload.php');
 
 		$this->org1 = $this->CreateOrganization("org1");
 		$this->org2 = $this->CreateOrganization("org2");
@@ -25,9 +27,18 @@ class MFAAdminRuleServiceTest2 extends ItopDataTestCase {
 	public function CreateUserWithProfilesAndOrg(string $sLogin, int $iOrgId, $aProfiles=[]) {
 		$oPerson = $this->CreatePerson("$sLogin", $iOrgId);
 
-		$aProfileSet = [];
-		foreach ($aProfiles as $iProfId){
-			$aProfileSet[]="profileid:$iProfId";
+		$aProfileSet = null;
+		if ($this->count($aProfiles)!=0) {
+			foreach ($aProfiles as $iProfId) {
+				$oUserProfile = new \URP_UserProfile();
+				$oUserProfile->Set('profileid', $iProfId);
+				$oUserProfile->Set('reason', 'UNIT Tests');
+				if (is_null($aProfileSet)){
+					$aProfileSet = \DBObjectSet::FromObject($oUserProfile);
+				} else {
+					$aProfileSet->AddItem($oUserProfile);
+				}
+			}
 		}
 
 		$oUser = $this->createObject('UserLocal', array(
@@ -41,10 +52,10 @@ class MFAAdminRuleServiceTest2 extends ItopDataTestCase {
 	}
 
 	public function testNoExistingRule() {
-		$oOrgLessUser = $this->CreateContactlessUser("NoOrgUser", ItopDataTestCase::$aURP_Profiles['Service Desk Agent']);
+		$oOrgLessUser = $this->CreateContactlessUser("NoOrgUser", ItopDataTestCase::$aURP_Profiles['Service Desk Agent'], "ABCdefg@12345#");
 		$this->assertEquals([], MFAAdminRuleService::GetInstance()->GetAdminRulesByUserId($oOrgLessUser->GetKey()));
 
-		$oPortalUserInOrg2 = $this->CreateUserWithProfilesAndOrg("PortalUserInOrg2", $this->org2->GetKey(), ItopDataTestCase::$aURP_Profiles['Portal user']);
+		$oPortalUserInOrg2 = $this->CreateUserWithProfilesAndOrg("PortalUserInOrg2", $this->org2->GetKey(), [ItopDataTestCase::$aURP_Profiles['Portal user']]);
 		$this->assertEquals([], MFAAdminRuleService::GetInstance()->GetAdminRulesByUserId($oPortalUserInOrg2->GetKey()));
 	}
 }
