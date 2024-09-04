46e188d4e5c24fcd755109095ea7803c863656a0
Eric Espie <eric.espie@combodo.com>
2024-09-04 12:07:53 +0200

    Mail configuration screens

--- src/Controller/MFABaseController.php
+++ src/Controller/MFABaseController.php
@@ -7,6 +7,7 @@
 namespace Combodo\iTop\MFABase\Controller;
 
 use Combodo\iTop\Application\TwigBase\Controller\Controller;
+use Combodo\iTop\MFABase\Service\MFAUserSettingsService;
 use MetaModel;
 use UserRights;
 use utils;
@@ -22,12 +23,18 @@ class MFABaseController extends Controller
 		$aItems = explode(':', $sAction);
 		$sVerb = $aItems[0];
 		$sModeClass = $aItems[1];
-
 		$sUserId = UserRights::GetUserId();
-		$oUserSettings = MetaModel::NewObject($sModeClass, ['user_id' => $sUserId]);
-		$aParams['sURL'] = $oUserSettings->GetConfigurationURLForMyAccountRedirection();
+
+		if ($sVerb === 'delete') {
+			$oUserSettings = MFAUserSettingsService::GetInstance()->GetMFAUserSettings($sUserId, $sModeClass);
+			$oUserSettings->AllowDelete();
+			$oUserSettings->DBDelete();
+			$aParams['sURL'] = utils::GetAbsoluteUrlAppRoot().'pages/exec.php?exec_module=combodo-my-account&exec_page=index.php&exec_env=production#TwigBaseTabContainer=tab_MyAccountTabMFA';
+		} else {
+			$oUserSettings = MetaModel::NewObject($sModeClass, ['user_id' => $sUserId]);
+			$aParams['sURL'] = $oUserSettings->GetConfigurationURLForMyAccountRedirection();
+		}
 
 		$this->DisplayPage($aParams);
 	}
-
 }
