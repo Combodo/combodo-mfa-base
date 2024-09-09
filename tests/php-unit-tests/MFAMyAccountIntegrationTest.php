8e7472b313aba0813e89f93848cb77cefcc07454
odain <olivier.dain@combodo.com>
2024-09-09 11:43:41 +0200

    fix test

--- tests/php-unit-tests/MFAMyAccountIntegrationTest.php
+++ tests/php-unit-tests/MFAMyAccountIntegrationTest.php
@@ -83,9 +83,9 @@ class MFAMyAccountIntegrationTest extends AbstractMFATest {
 	public function testMyAccountMfaTab_DisplayMFATabContent()
 	{
 		// Arrange
-		$oActiveSetting1 = $this->CreateSetting('MFAUserSettingsTOTPApp', $this->oUser->GetKey(), 'yes', [], true);
-		$oActiveSetting2 = $this->CreateSetting('MFAUserSettingsTOTPMail', $this->oUser->GetKey(), 'yes', [], true);
-		$oActiveSetting3 = $this->CreateSetting('MFAUserSettingsRecoveryCodes', $this->oUser->GetKey(), 'yes', [], true);
+		//$oActiveSetting1 = $this->CreateSetting('MFAUserSettingsTOTPApp', $this->oUser->GetKey(), 'yes', [], true);
+		//$oActiveSetting2 = $this->CreateSetting('MFAUserSettingsTOTPMail', $this->oUser->GetKey(), 'yes', [], true);
+		//$oActiveSetting3 = $this->CreateSetting('MFAUserSettingsRecoveryCodes', $this->oUser->GetKey(), 'yes', [], true);
 
 		// Act
 		$sOutput = $this->CallItopUrl('/pages/exec.php?exec_module=combodo-my-account&exec_page=index.php&operation=MyAccountTab&exec_env=production&tab=MyAccount%3ATab%3AMFA',
