cd539ace32c122bf2861780d9384102810ed7889
Eric Espie <eric.espie@combodo.com>
2024-09-23 14:57:34 +0200

    Changed the menus to be dynamic (i.e. not visible when MFA is disabled)

---
+++ src/Hook/MFABaseMenu.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @copyright   Copyright (C) 2010-2024 Combodo SARL
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+namespace Combodo\iTop\MFABase\Hook;
+
+use ApplicationMenu;
+use Combodo\iTop\MFABase\Helper\MFABaseConfig;
+use ModuleHandlerAPI;
+use OQLMenuNode;
+use ResourceMFAMenu;
+use UserRights;
+
+class MFABaseMenu extends ModuleHandlerAPI
+{
+
+	public static function OnMenuCreation()
+	{
+		if (MFABaseConfig::GetInstance()->IsEnabled() && UserRights::IsActionAllowed(ResourceMFAMenu::class, UR_ACTION_MODIFY)) {
+			new OQLMenuNode(
+				'MFAAdminRule',
+				'SELECT MFAAdminRule',
+				ApplicationMenu::GetMenuIndexById('UserManagement'),
+				40,
+				true);
+			new OQLMenuNode(
+				'MFAUserSettings',
+				'SELECT MFAUserSettings',
+				ApplicationMenu::GetMenuIndexById('UserManagement'),
+				50,
+				true);
+		}
+	}
+}
