fab6ba2ec34d7916f083f291a55ee2a31f30d532
Eric Espie <eric.espie@combodo.com>
2024-09-30 17:30:47 +0200

    Portal user profile brick

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -23,20 +23,11 @@ class ComposerAutoloaderInit82e29b7fbd0d36d303017d22d2ca07ee
         }
 
         spl_autoload_register(array('ComposerAutoloaderInit82e29b7fbd0d36d303017d22d2ca07ee', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit82e29b7fbd0d36d303017d22d2ca07ee', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
-        if ($useStaticLoader) {
-            require __DIR__ . '/autoload_static.php';
-
-            call_user_func(\Composer\Autoload\ComposerStaticInit82e29b7fbd0d36d303017d22d2ca07ee::getInitializer($loader));
-        } else {
-            $classMap = require __DIR__ . '/autoload_classmap.php';
-            if ($classMap) {
-                $loader->addClassMap($classMap);
-            }
-        }
+        require __DIR__ . '/autoload_static.php';
+        call_user_func(\Composer\Autoload\ComposerStaticInit82e29b7fbd0d36d303017d22d2ca07ee::getInitializer($loader));
 
         $loader->setClassMapAuthoritative(true);
         $loader->register(true);
